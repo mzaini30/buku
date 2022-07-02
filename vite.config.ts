f89ac0f96596f5ad485c02f1f519f41d50e7ad60
mzaini30 <muhzaini30@gmail.com>
2022-07-02 16:39:00 +0800

    tambah adsense

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import {defineConfig} from "vite"
 export default defineConfig({
 	plugins: [book({
 		name: "Buku Zenia",
-		repo: "mzaini30/buku"
+		repo: "https://github.com/mzaini30/buku"
 	})]
 })
